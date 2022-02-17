eee738a684e3c3defc72a7e3340c1bbafec9a236
pagnani <andrea.pagnani@gmail.com>
2022-02-17 14:08:20 +0100

    allowscalar -> false

--- src/BpAlignGpu.jl
+++ src/BpAlignGpu.jl
@@ -1,9 +1,12 @@
 module BpAlignGpu
 
+CUDA.allowscalar(false)
+
 using Tullio, CUDA, LoopVectorization, CUDAKernels, KernelAbstractions
 using ExtractMacro: @extract
 #export crea_instance_tensor_long, test_instance_tensor_long!
 export BPMessages, BPBeliefs, LongRangeFields, AllFields, Seq, ParamModel, ParamAlgo
+
 
 include("types.jl")
 include("bpupdate.jl")
