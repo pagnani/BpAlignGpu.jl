96858632f09c1beb36506b652ae4f77949a1377c
pagnani <andrea.pagnani@gmail.com>
2022-03-07 14:50:55 +0100

    added LinearAlgebra dep

--- src/BpAlignGpu.jl
+++ src/BpAlignGpu.jl
@@ -6,6 +6,7 @@ using Tullio, CUDA, LoopVectorization, CUDAKernels, KernelAbstractions
 CUDA.allowscalar(false)
 using ExtractMacro: @extract
 export BPMessages, BPBeliefs, LongRangeFields, AllFields, Seq, ParamModel, ParamAlgo
+using LinearAlgebra: diag
 
 include("dataset.jl") #copy-paste of DCAlign-master
 include("types.jl")
